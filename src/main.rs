3ff07967cbf3f45eb677124d45def3a22fa66dbe
Arpan Dhatt <ary.dhatt@gmail.com>
2022-11-19 22:48:50 -0600

    sorta broken image baking (first attempt breaks, but later ones succeed??)

--- src/main.rs
+++ src/main.rs
@@ -1,87 +1,230 @@
-use std::env::args;
+use std::{collections::VecDeque, env::args, mem, ops::DerefMut, sync::Mutex};
 
-use nannou::prelude::*;
+use lazy_static::lazy_static;
+use nannou::{
+    color::encoding::Srgb,
+    image::{imageops::FilterType, io::Reader as ImageReader, GenericImageView, Pixel},
+    prelude::*,
+};
+use rand::{rngs::SmallRng, Rng, SeedableRng};
+
+lazy_static! {
+    static ref RNG: Mutex<SmallRng> = Mutex::new(SmallRng::from_seed([5; 16]));
+}
 
 struct Model {
-    balls: Vec<Ball>
+    image: String,
+    colors: VecDeque<rgb::Rgb<Srgb, u8>>,
+    balls: Vec<Ball>,
+    emitters: Vec<Emitter>,
 }
 
 #[derive(Clone, Debug)]
 struct Ball {
-    x: f32,
-    y: f32,
-    x1: f32,
-    y1: f32,
-    r: f32
+    p: Vec2,
+    p1: Vec2,
+    r: f32,
+    c: rgb::Rgb<Srgb, u8>,
+}
+
+#[derive(Clone, Debug)]
+struct Emitter {
+    count: usize,
+    theta: f32,
+    rv: f32,
+}
+
+const PERIOD: usize = 1;
+const POWER: f32 = 8.0;
+
+impl Emitter {
+    fn update(&mut self) {
+        self.theta += self.rv;
+    }
+
+    fn emit(&mut self, colors: &mut VecDeque<rgb::Rgb<Srgb, u8>>) -> Option<Ball> {
+        self.count += 1;
+        if self.count % PERIOD == 0 && !colors.is_empty() {
+            let (x, y) = (
+                (RAD - 8.0) * self.theta.cos(),
+                (RAD - 8.0) * self.theta.sin(),
+            );
+            let (x1, y1) = (
+                (RAD - 8.0 + POWER) * self.theta.cos(),
+                (RAD - 8.0 + POWER) * self.theta.sin(),
+            );
+            return Some(Ball {
+                p: vec2(x, y),
+                p1: vec2(x1, y1),
+                r: RNG.lock().unwrap().gen_range(4.0, 10.0),
+                c: colors.pop_front().unwrap(),
+            });
+        } else {
+            None
+        }
+    }
 }
 
 const WIDTH: f32 = 1024.0;
 const HEIGHT: f32 = 1024.0;
-const GRAV: f32 = -980.0;
+const RAD: f32 = 480.0;
+const GRAV: f32 = -9800.0;
 
 fn main() {
     nannou::app(model).update(update).run();
 }
 
 fn model(app: &App) -> Model {
-    app.new_window().size(WIDTH as u32, HEIGHT as u32).event(event).view(view).build().unwrap();
-    Model { balls: (0..args().into_iter().nth(1).unwrap().parse().unwrap()).map(|_| rand_ball()).collect() }
+    app.new_window()
+        .size(WIDTH as u32, HEIGHT as u32)
+        .event(event)
+        .view(view)
+        .build()
+        .unwrap();
+    Model {
+        image: args().nth(1).unwrap(),
+        colors: (0..4000).into_iter().map(|_| rgb(255, 255, 255)).collect(),
+        balls: vec![],
+        emitters: (0..4).into_iter().map(|_| rand_emitter()).collect(),
+    }
 }
 
-fn rand_ball() -> Ball {
-    let x = random::<f32>() * WIDTH - WIDTH/2.0;
-    let y = random::<f32>() * HEIGHT - HEIGHT/2.0;
-    Ball {
-        x, y, x1: x, y1: y,
-        r: random::<f32>() + 5.0
-    }
+fn rand_emitter() -> Emitter {
+    let mut rng = RNG.lock().unwrap();
+    let theta = rng.gen_range(0.0, PI * 2.0);
+    return Emitter {
+        theta,
+        rv: rng.gen_range(4.0 / PI / 60.0, 6.0 / PI / 60.0),
+        count: 0,
+    };
 }
 
 fn event(app: &App, model: &mut Model, event: WindowEvent) {
     match event {
-        MousePressed(_) => {
-            model.balls.push(Ball {
-                x: app.mouse.x,
-                y: app.mouse.y,
-                x1: app.mouse.x,
-                y1: app.mouse.y,
-                r: 10.0
-            });
+        KeyPressed(Key::Return) => {
+            {
+                let mut rng = RNG.lock().unwrap();
+                let _ = mem::replace(rng.deref_mut(), SmallRng::from_seed([0; 16]));
+                println!("replaced rng");
+            }
+            get_colors(model);
+            *model = Model {
+                image: model.image.clone(),
+                colors: get_colors(model),
+                balls: vec![],
+                emitters: (0..4).into_iter().map(|_| rand_emitter()).collect(),
+            };
+            println!("replaced model");
         }
         _ => {}
     }
 }
 
+fn get_colors(model: &mut Model) -> VecDeque<rgb::Rgb<Srgb, u8>> {
+    let image = ImageReader::open(&model.image)
+        .unwrap()
+        .decode()
+        .unwrap()
+        .resize_exact(WIDTH as u32, HEIGHT as u32, FilterType::Lanczos3);
+    model
+        .balls
+        .iter()
+        .map(|b| {
+            let (i, j) = coord_to_pix(b.p);
+            let mut col = (0, 0, 0);
+            let mut count = 0;
+            for k in (-b.r as i32)..(b.r as i32) {
+                let y = (b.r * b.r - (k * k) as f32).sqrt() as i32;
+                for l in (-y)..y {
+                    let (x, y) = (i + k, j + l);
+                    if x >= 0 && x < WIDTH as i32 && y >= 0 && y < HEIGHT as i32 {
+                        let temp = image.get_pixel(x as u32, y as u32);
+                        col.0 += temp.0[0] as u32;
+                        col.1 += temp.0[1] as u32;
+                        col.2 += temp.0[2] as u32;
+                        count += 1;
+                    }
+                }
+            }
+            if count > 0 {
+                rgb(
+                    (col.0 / count) as u8,
+                    (col.1 / count) as u8,
+                    (col.2 / count) as u8,
+                )
+            } else {
+                rgb(0, 0, 0)
+            }
+        })
+        .collect()
+    // model.balls.iter_mut().for_each(|b| {
+    //     b.c = {
+    //         let (i, j) = coord_to_pix(b.p);
+    //         let col = image.get_pixel(i, j);
+    //         rgb(col.0[0], col.0[1], col.0[2])
+    //     }
+    // });
+}
+
+fn coord_to_pix(c: Vec2) -> (i32, i32) {
+    (
+        (c.x as i32 + WIDTH as i32 / 2).clamp(0, WIDTH as i32 - 1),
+        (-c.y as i32 + HEIGHT as i32 / 2).clamp(0, HEIGHT as i32 - 1),
+    )
+}
+
 fn step(model: &mut Model, mut delta: f32, substeps: usize) {
+    let grav = vec2(0.0, GRAV);
     delta /= substeps as f32;
     for _ in 0..substeps {
         for i in 0..model.balls.len() {
             let (front, back) = model.balls.split_at_mut(i + 1);
-            let ball_a = unsafe { front.last_mut().unwrap_unchecked() };
-            (ball_a.x, ball_a.x1) = (2.0*ball_a.x - ball_a.x1 + 0.0f32, ball_a.x);
-            (ball_a.y, ball_a.y1) = (2.0*ball_a.y - ball_a.y1 + GRAV * delta * delta, ball_a.y);
-
-            ball_a.x = ball_a.x.clamp(-WIDTH/2.0 + ball_a.r, WIDTH/2.0 - ball_a.r);
-            ball_a.y = ball_a.y.clamp(-HEIGHT/2.0 + ball_a.r, HEIGHT/2.0 - ball_a.r);
+            let mut ball_a = unsafe { front.last_mut().unwrap_unchecked() };
+            (ball_a.p, ball_a.p1) = (2.0 * ball_a.p - ball_a.p1 + grav * delta * delta, ball_a.p);
+            // ball_a.x = ball_a
+            //     .x
+            //     .clamp(-WIDTH / 2.0 + ball_a.r, WIDTH / 2.0 - ball_a.r);
+            // ball_a.y = ball_a
+            //     .y
+            //     .clamp(-HEIGHT / 2.0 + ball_a.r, HEIGHT / 2.0 - ball_a.r);
+            clamp_circle(&mut ball_a);
 
             for ball_b in back {
-                let diff = (ball_b.x - ball_a.x, ball_b.y - ball_a.y);
-                let dist = ((diff.0 * diff.0) + (diff.1 * diff.1)).sqrt();
-                if dist < (ball_a.r + ball_b.r) {
-                    let norm = (diff.0 / dist, diff.1 / dist);
-                    ball_a.x -= 0.5f32 * (((ball_a.r + ball_b.r) - dist) * norm.0);
-                    ball_a.y -= 0.5f32 * (((ball_a.r + ball_b.r) - dist) * norm.1);
+                let diff = ball_b.p - ball_a.p;
+                let limit = ball_a.r + ball_b.r;
+                // check manhatten distance first
+                // if diff.0.abs() > limit && diff.1.abs() > limit {
+                //     continue;
+                // }
+                let dist = diff.length();
+                if dist < limit {
+                    let norm = diff.normalize();
+                    let part = (ball_a.r * ball_a.r) / (ball_a.r * ball_a.r + ball_b.r * ball_b.r);
+                    ball_a.p -= (1.0 - part) * ((limit - dist) * norm);
 
-                    ball_b.x += 0.5f32 * (((ball_a.r + ball_b.r) - dist) * norm.0);
-                    ball_b.y += 0.5f32 * (((ball_a.r + ball_b.r) - dist) * norm.1);
+                    ball_b.p += part * ((limit - dist) * norm);
                 }
             }
         }
     }
 }
 
+fn clamp_circle(ball: &mut Ball) {
+    let rad = ball.p.length() + ball.r;
+    if rad > RAD {
+        ball.p += -ball.p.normalize() * (rad - RAD);
+    }
+}
+
 fn update(_app: &App, model: &mut Model, _update: Update) {
-    step(model, 0.016, 8);
+    if !model.colors.is_empty() {
+        let new_balls = model.emitters.iter_mut().filter_map(|e| {
+            e.update();
+            e.emit(&mut model.colors)
+        });
+        model.balls.extend(new_balls);
+    }
+    step(model, 0.001, 4);
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
@@ -89,14 +232,13 @@ fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
 
     // Clear the background to purple.
-    draw.background().color(PLUM);
+    draw.background().color(BLACK);
 
     // Draw a blue ellipse with default size and position.
     for ball in &model.balls {
-        draw
-            .ellipse()
-            .color(STEELBLUE)
-            .x_y(ball.x, ball.y)
+        draw.ellipse()
+            .color(ball.c)
+            .x_y(ball.p.x, ball.p.y)
             .radius(ball.r);
     }
 
